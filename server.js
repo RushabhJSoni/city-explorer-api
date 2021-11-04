6e2e6f0abc3e99d6011669d69de913d2489ba6fd
rushabhjsoni <slayer.r1@gmail.com>
2021-11-05 05:16:25 +0530

    completed lab 8

--- server.js
+++ server.js
@@ -2,42 +2,36 @@
 
 
 require('dotenv').config();
-const express = require('express');
-const weather = require('./data/weather.json');
+const app = require('express')();
 const axios = require('axios');
 const cors = require('cors');
-const app = express();
-
 app.use(cors());
-const PORT = process.env.PORT || 3001
+
+const PORT = process.env.PORT 
+
 app.get('/weather', handleGetWeather);
-// app.get('/*' , (req,res) => res.status(403).send('not found'))
 
-function handleGetWeather(req,res){
-  const { lat, lon } = req.query;
-  const url = `http://api.weatherbit.io/v2.0/current?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
-  const cityName = req.query.city;
-  const lat = req.query.lat;
-  const lon = req.query.lon;
-  const cityToSend =weather.find(city=>{
-    if((city.city_name === cityName)){
-      return true;
-    } return false;
-  });
-  if(cityToSend){
-    const forecastData =cityToSend.data.map(city=> new Weatherforecast(city));
-res.send(forecastData);
-}else{
-  res.send.status(404);
-}
-}
+app.get('/*' , (req,res) => res.status(403).send('not found'))
 
+async function handleGetWeather(req,res){
+  const {lat,lon} = req.query;
+  try {
+    let response = await axios.get(`http://api.weatherbit.io/v2.0/current?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`);
+
+    let weatherData = response.data.data.map(data => new Weatherforecast(data));
+    res.status(200).send(weatherData);
+  } catch(error) {
+      res.status(500).send('server error')
+  }
+}
+ 
 class Weatherforecast {
-  constructor(obj){
-    this.min_temp= obj.min_temp;
-    this.max_temp=obj.max_temp;
-    this.description = obj.weather.description;
+  constructor(obj) {
+    this.vis = obj.vis;
+    this.wind_cdir = obj.wind_cdir;
+    this.wind_spd = obj.wind_spd;
   }
 }
 
 app.listen(PORT, () => console.log(`I am a server that is listening on port:${PORT}`));
+
